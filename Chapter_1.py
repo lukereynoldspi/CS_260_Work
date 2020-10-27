6a113d082e449a95b3516492da748a809044d53e
Luke Reynolds <31745084+lukereynoldspi@users.noreply.github.com>
2020-10-27 02:37:46 -0700

    worked on exercises

--- Chapter_1.py
+++ Chapter_1.py
@@ -4,3 +4,67 @@ class Fraction:
 
         self.num = top
         self.den = bottom
+
+
+    def show(self):
+        print(self.num, "/", self.den)
+
+    def __str__(self):
+        return str(self.num) + "/" + str(self.den)
+
+    def __add__(self, otherfraction):
+        newnum = self.num * otherfraction.den + self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
+
+    def __add__(self, otherfraction):
+        newnum = self.num * otherfraction.den + self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+        common = gcd(newnum, newden)
+        return Fraction(newnum // common, newden // common)
+
+    def __eq__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+
+        return firstnum == secondnum
+
+    def gcd(m, n):
+        while m % n != 0:
+            oldm = m
+            oldn = n
+
+            m = oldn
+            n = oldm % oldn
+        return n
+
+    def getNum(self):
+        return self.num
+
+    def getDen(self):
+        return self.den
+
+    def __sub__(self, otherfraction):
+        newnum = self.num * otherfraction.num
+        newdem = self.den * otherfraction.den
+
+        return Fraction(newnum,newdem)
+
+    def __mul__(self, otherfraction):
+        newnum = self.num * otherfraction.num
+        newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
+
+
+    def __truediv__(self, otherfraction):
+        newnum = self.num * otherfraction.den
+        newden = self.den * otherfraction.num
+
+        return Fraction(newnum, newden)
+
+e = Fraction(1,2)
+f = Fraction(1,4)
+
+print(e.__truediv__(f))
